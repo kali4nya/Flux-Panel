0ccd26eac6a63713949e13a22b8409734d2d879e
kali4 <64191123+kali4nya@users.noreply.github.com>
2026-02-28 10:25:04 +0100

    Update cpu.py

--- stats/cpu.py
+++ stats/cpu.py
@@ -5,6 +5,6 @@ psutil.cpu_percent()  # Ignore first call
 
 def get_cpu_stats():
     return {
-        "usage_percent": psutil.cpu_percent(interval=0.1),  # small blocking interval
+        "usage_percent": psutil.cpu_percent(interval=0.2),  # small blocking interval
         "cores": psutil.cpu_count()
     }
